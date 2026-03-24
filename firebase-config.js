d7ac4dbc375957dbdc38723ba7dcc5a18f266230
padillahernandezcristianfabian-hub <padillahernandezcristianfabian@gmail.com>
2026-03-24 05:39:57 -0600

    Create firebase-config.js
    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+firebase-config.jsconst firebaseConfig = {
+  apiKey: "AIzaSyA8sPy5IVXdysBvhgiTD2Q7ZTQ4C8Zlj84",
+  authDomain: "zenitsystempro-bf80f.firebaseapp.com",
+  projectId: "zenitsystempro-bf80f",
+  storageBucket: "zenitsystempro-bf80f.firebasestorage.app",
+  messagingSenderId: "627297810873",
+  appId: "1:627297810873:web:f3bb4cd79ead7939d55e68"
+};
